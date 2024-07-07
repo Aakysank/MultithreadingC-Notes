93a2208b70525327dda73f13e0ca461713a7b651
Aakash Sankaran <30946472+Aakysank@users.noreply.github.com>
2024-07-07 21:59:42 +0530

    Create timedmutex.cpp

---
+++ timedmutex.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<thread>
+#include<mutex>
+
+std::mutex mt;
+void task1()
+{
+  std::cout << "Task 1 is trying to lock the mutex" << std::endl;
+  
+  mutex.lock();
+  std::cout << "Task 1 has successfully locked the mutex" << std::endl;
+  mutex.unlock();
+  std::cout << "Task 1 has successfully unlocked the mutex" << std::endl;
+  return;
+}
+
+void task2()
+{
+  std::cout << "Task 2 is trying to lock the mutex" << std::endl;
+  while (!mutex.try_lock_for(std::chrono::seconds(1)))
+    std::cout << "Task 2 is unable to lock the mutex" << std::endl;
+
+  std::cout << "Task 2 has successfully locked the mutex" << std::endl;
+  mutex.unlock();
+  std::cout << "Task 2 has successfully unlocked the mutex" << std::endl;
+  return;
+}
+
+int main()
+{
+  std::thread t1(task1), t2(task2);
+  t1.join();
+  t2.join();
+  return 0;
+}
